f0ff959f5b00fa4aa11383951435e0a9b78a84ff
Julian.Willingmann <jwillingmann@web.de>
2018-01-27 15:49:12 +0100

    added delete method

--- app/src/main/java/com/example/nicolasschilling/intentsvorlage2/FileWriter.java
+++ app/src/main/java/com/example/nicolasschilling/intentsvorlage2/FileWriter.java
@@ -1,9 +1,10 @@
 package com.example.nicolasschilling.intentsvorlage2;
 
 import android.os.Environment;
+import android.provider.DocumentsContract;
 import android.util.Log;
 import android.widget.Toast;
-
+import java.io.*;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
@@ -11,6 +12,7 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -22,44 +24,40 @@ import static android.content.ContentValues.TAG;
  */
 
 public class FileWriter {
-    final static String fileName ="file.txt";
+    final static String fileName = "file.txt";
     final static String path = "NotizAppDirectory";
 
 
+    public boolean saveFile(String filedata) {
 
 
-    public boolean saveFile(String filedata){
-
-
-        try{
+        try {
 
             File directory = new File(Environment.getExternalStorageDirectory(), this.path);
 
 
             //Checking for existing directory, else constructing one
-            if(!directory.mkdirs())
-            {
+            if (!directory.mkdirs()) {
                 System.out.println("Created a Directory ...");
             }
 
 
-            File file = new File (directory, this.fileName);
+            File file = new File(directory, this.fileName);
+
             //Create File if needed
             try {
                 file.createNewFile();
                 System.out.println("Created File!");
-            }catch (Exception e){
+            } catch (Exception e) {
                 System.out.println("Failed to create correct file: " + e);
             }
-
-
 
 
             System.out.println("Opening file stream");
             //Writing the String filedata on storage
             FileOutputStream fileOutputStream = new FileOutputStream(file, true);
             System.out.println("Writing bytes");
-            fileOutputStream.write((filedata +"\n").getBytes());
+            fileOutputStream.write((filedata + "\n").getBytes());
             System.out.println("Closing");
             //Closing the file
             fileOutputStream.close();
@@ -67,18 +65,15 @@ public class FileWriter {
 
             System.out.println("Wrote the file");
 
-        }catch (FileNotFoundException ex) {
+        } catch (FileNotFoundException ex) {
             Log.d(TAG, ex.getMessage());
             return false;
-        } catch (IOException ex){
+        } catch (IOException ex) {
             Log.d(TAG, ex.getMessage());
             return false;
         }
         return true;
     }
-
-
-
 
 
     String readFile(String fileName) {
@@ -107,9 +102,35 @@ public class FileWriter {
     }
 
 
+    public void delete(int positon) {
+        try {
+            File directory = new File(Environment.getExternalStorageDirectory(), this.path);
+            File file = new File(directory, fileName);
+            File nfile = new File(directory, "nfile.txt");
+            BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(nfile)));
+            int counter = 0;
+            String line = null;
+            while ((line = br.readLine()) != null) {
+                if (counter != positon) {
+                    bw.write(line);
+                    bw.newLine();
+
+                }
+                counter++;
+            }
+            bw.close();
+            br.close();
+            file.delete();
+            nfile.renameTo(file);
 
 
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
 
-
+    }
 }
