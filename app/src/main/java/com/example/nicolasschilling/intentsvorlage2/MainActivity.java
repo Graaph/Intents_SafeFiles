09a37a6241fe638a70c3fbec444c659fc8684b99
Graaph <nicolas.c.schilling@gmail.com>
2018-01-27 14:01:28 +0100

    Useless stuff deleted

--- app/src/main/java/com/example/nicolasschilling/intentsvorlage2/MainActivity.java
+++ app/src/main/java/com/example/nicolasschilling/intentsvorlage2/MainActivity.java
@@ -4,9 +4,13 @@ package com.example.nicolasschilling.intentsvorlage2;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ShareActionProvider;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -34,6 +38,20 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+//Share-Funktion
+    public void share(String sharetxt){
+        Intent sendIntent = new Intent();
+        sendIntent.setAction(Intent.ACTION_SEND);
+        sendIntent.putExtra(Intent.EXTRA_TEXT, "This is my text to send.");
+        sendIntent.setType("text/plain");
+        startActivity(sendIntent);
+
+    }
+
+
+
+
+
 
 
     //Filesave button
@@ -49,6 +67,9 @@ public class MainActivity extends AppCompatActivity {
         } else {
             Toast.makeText(MainActivity.this, "ERROR", Toast.LENGTH_SHORT).show();
         }
+
+
+        editText.setText("");
 
         Intent intent = new Intent(this, ShowMe.class);
             startActivity(intent);
